5b4dc54d32ee7f179bbc59ee0816945a82acece9
zachary-o <104291766+zachary-o@users.noreply.github.com>
2023-03-05 00:09:41 +0200

    add wishlist page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Wishlist from "./pages/Wishlist/Wishlist";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/wishlist" element={<Wishlist />} />
       </Routes>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import "./Header.scss"
 
@@ -9,13 +10,13 @@ const Header = () => {
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
-          <a href="/">
+          <Link to="/">
             <img src={logo} alt="Logotype" />
-          </a>
+          </Link>
           <div className="navbar">
             <ul>
               <li>
-                <a href="/" className="navbar__icon">
+                <Link to="/wishlist" className="navbar__icon">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -30,7 +31,7 @@ const Header = () => {
                       d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
                     />
                   </svg>
-                </a>
+                </Link>
               </li>
               <li>
                 <a href="/" className="navbar__icon">
